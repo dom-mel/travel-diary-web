862c71b3b7a5b8c1f543818a37c263c16d0fdd87
Dominik Eckelmann <deckelmann@gmail.com>
2013-10-08 16:29:10 +0200

    added upload view

--- js/app.js
+++ js/app.js
@@ -3,10 +3,10 @@ var travelDiary = angular.module('travelDiary',['ngSanitize']);
 
 travelDiary.config(['$routeProvider', function($routeProvider) {
     $routeProvider
-        .when('/',             {templateUrl: 'tpl/home.htm',   controller: HomeCtrl})
-        .when('/upload',       {templateUrl: 'tpl/upload.htm', controller: UploadCtrl})
-        .when('/create',       {templateUrl: 'tpl/create.htm', controller: CreateCtrl})
-        .when('/diary/:id',    {templateUrl: 'tpl/diary.htm',  controller: DiaryCtrl})
+        .when('/',                 {templateUrl: 'tpl/home.htm',   controller: HomeCtrl})
+        .when('/create',           {templateUrl: 'tpl/create.htm', controller: CreateCtrl})
+        .when('/diary/:id',        {templateUrl: 'tpl/diary.htm',  controller: DiaryCtrl})
+        .when('/diary/:id/upload', {templateUrl: 'tpl/upload.htm', controller: UploadCtrl})
         .otherwise({redirectTo: '/'});
 }]);
 
--- js/controllers/diary.js
+++ js/controllers/diary.js
@@ -1,11 +1,12 @@
 'use strict';
 
-function DiaryCtrl($scope, apiService, $routeParams) {
+function DiaryCtrl($scope, apiService, $routeParams, $location) {
 
     apiService.diary.get($routeParams.id).then(function(result) {
         $scope.diary = result.data.diary;
+        $scope.upload = function() {
+            $location.path('/diary/' + $scope.diary.id + '/upload');
+        }
     });
-
-
 
 }
