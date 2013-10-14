ce2ca8f00fd3d962bee786bc3fd79d05aae807f6
Dominik Eckelmann <deckelmann@gmail.com>
2013-10-14 21:03:25 +0200

    changed upload form, swtiched to gmaps

--- js/controllers/diary.js
+++ js/controllers/diary.js
@@ -9,4 +9,23 @@ function DiaryCtrl($scope, apiService, $routeParams, $location) {
         }
     });
 
+    var mapOptions = {
+        center: new google.maps.LatLng(-34.397, 150.644),
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        zoomControl: false,
+        panControl: false,
+        draggable: false,
+        scrollwheel: false,
+        keyboardShortcuts: false,
+        rotateControl: false,
+        disableDoubleClickZoom: true
+
+    };
+    var map = new google.maps.Map(document.getElementById("diaryMap"),
+        mapOptions);
+
 }
--- js/controllers/upload.js
+++ js/controllers/upload.js
@@ -1,15 +1,39 @@
 'use strict';
 
-function UploadCtrl() {
+function UploadCtrl($scope) {
 
-    var map = new OpenLayers.Map( 'map');
-    var layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
-    map.addLayer(layer);
-    map.setCenter(
-        new OpenLayers.LonLat(-71.147, 42.472).transform(
-            new OpenLayers.Projection("EPSG:4326"),
-            map.getProjectionObject()
-        ), 12
-    );
+    var mapOptions = {
+        center: new google.maps.LatLng(30, 0),
+        zoom: 1,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        streetViewControl: false
+    };
+    var map = new google.maps.Map(document.getElementById("uploadMap"), mapOptions);
 
+    var geoCoder = new google.maps.Geocoder();
+    var marker = new google.maps.Marker();
+    marker.setDraggable(true);
+
+    $scope.selectLocation = function() {
+
+        geoCoder.geocode( { 'address': $scope.location}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                map.setCenter(results[0].geometry.location);
+                marker.setPosition(results[0].geometry.location);
+                map.setZoom(5);
+                marker.setMap(map);
+            } else {
+                alert("Geocode was not successful for the following reason: " + status);
+            }
+        });
+    };
+
+    $scope.resetMap = function() {
+        map.setCenter(new google.maps.LatLng(30, 0));
+        map.setZoom(1);
+        $scope.location = "";
+        if (marker !== undefined) {
+            marker.setMap(null);
+        }
+    };
 }
